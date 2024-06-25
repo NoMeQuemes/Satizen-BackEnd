82fa7bce4befb46ebc9cf32021a6dc9938dffe6a
leandro01echevarria <leandroechevarria0107@gmail.com>
2024-06-25 13:23:48 -0300

    Se agrega dispositivo laboral

--- obj/Debug/net7.0/Satizen_Api.AssemblyInfo.cs
+++ obj/Debug/net7.0/Satizen_Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Satizen_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c226b501b4c1bb9aeeeef4056e267d7bfbe7bbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b246e14f4a1b0ac77dc8900fe7249bf5982be53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Satizen_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Satizen_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
